2a94c12143641613c6b204bbf2ad54452c7a02c9
zevin02 <1833195090@qq.com>
2022-03-03 00:32:51 +0800

    介绍了一点fork内容

--- 初始进程/myproc.c
+++ 初始进程/myproc.c
@@ -7,14 +7,33 @@
 //1。标识符pid
 //
 
+void fork_test()
+{
+int ret=fork();//fork可以创建子进程
+while(1)
+{
+    printf("i am a process %d,%d\n",getpid(),getppid());
+    sleep(1);
+}
 
+}
+
+void pcb()
+{
+    
+//     while(1)
+//    {
+//        //printf("hello \n");
+//        printf("hello pid= %d,ppid=%d\n",getpid(),getppid());
+//        sleep(1);
+}
 int main()
 {
-while(1)
-    {
-        //printf("hello \n");
-        printf("hello pid= %d,ppid=%d\n",getpid(),getppid());
-        sleep(1);
-    }
+    fork_test();
+    fork();
+    printf("hello");
+
+
+//    }
     return 0;//退出码，
 }
