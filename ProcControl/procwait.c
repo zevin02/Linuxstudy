c856ffef7bfb53feeee7cc232634109aa503732c
zevin02 <1833195090@qq.com>
2022-03-27 15:47:48 +0800

    waitpid的理解以及status的认识

--- ProcControl/procwait.c
+++ ProcControl/procwait.c
@@ -17,19 +17,26 @@ int main()
       //子进程会持续5秒之后就exit，但是父进程；立刻就退出了
       //子进程就会变成一个孤儿进程，被系统领养
     }
-    exit(0);//我们执行完子进程之后直接退出，不执行父进程的代码
+    exit(10);//我们执行完子进程之后直接退出，不执行父进程的代码
     //进程退出
+    //父进程拿到司马status结果，一等和子进程如何退出强相关！！！！
+    //子进程退出的话题，就是我们刚讲过的进程退出的话题
   }
-  sleep(10);//先让父进程等待10秒
+  //sleep(10);//先让父进程等待10秒
   //前5秒是正常运行的
   //后5秒子进程处于z状态
   printf("father wait begin\n");
   //因为父进程和子进程是同时执行的
   //parent
   //为了不让子进程变成一个孤儿进程，父进程必须在那里等待
-  pid_t ret=wait(NULL);//成功的话就返回其
+  //pid_t ret=wait(NULL);//成功的话就返回其
+  //pid_t ret=waitpid(id,NULL,0);//第一个是其等待的进程的pid
+  //没有任何区别
+  //pid_t ret=waitpid(-1,NULL,0);//id=-1，就是等待任意一个子进程，如果有很多子进程，他就只会等待任意一个子进程
+  int status=0;
+  pid_t ret=waitpid(id,&status,0);//第一个是其等待的进程的pid
   if(ret>0)
-    printf("father wait %d\n ",ret);
+    printf("father wait %d, status: %d \n ",ret,status);//可以获得这个输出性参数
   else 
     printf("father wait fail\n");
   //到这里僵尸状态就没了
