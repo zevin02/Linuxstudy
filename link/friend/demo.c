0f9ce759e48ff8d87a58058f9de7c10b1004ed51
zevin02 <1833195090@qq.com>
2022-04-20 22:17:01 +0800

    master the creativity and postment of dynamic/static library

---
+++ link/friend/demo.c
@@ -0,0 +1,11 @@
+#include"add.h"
+#include"sub.h"
+
+int main()
+{
+    int x=10;
+    int y=2;
+    int y1=add(x,y);
+    printf("y1=%d" ,y1);
+    return 0;
+}
---
+++ link/other/lib/lib/sub.h
@@ -0,0 +1,4 @@
+
+#pragma once 
+#include <stdio.h>
+extern int sub(int x,int y);//声明
