ae379769b3d3d7995d3f1e1af8927f048de30896
zevin02 <1833195090@qq.com>
2022-04-25 21:36:39 +0800

    测试了匿名管道读写不同步

---
+++ procommunicate/demo.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include <string.h>
+#include<sys/wait.h>
+void wmorer()
+{
+  int fd[2];
+  int ret=pipe(fd);
+  if(ret==-1)
+  {
+    perror("pipe");
+    exit(0);
+  }
+  pid_t id=fork();
+  if(id==-1)
+  {
+    perror("fork");
+    exit(0);
+  }
+  else if(id==0)
+  {
+    //child
+    //write
+    close(fd[0]);
+    const char* buf="hello world";
+    while(1)
+    {
+      write(fd[1],buf,sizeof(buf));
+      printf("child is sending message\n");
+      sleep(5);
+    }
+  }
+  else 
+  {
+    //father
+    close(fd[1]);
+    while(1)
+    {
+     // sleep(1);
+      char buffer[32];
+      ssize_t s=read(fd[0],buffer,sizeof(buffer));
+      if(s>0)
+      {
+        buffer[s]=0;
+        printf("read message :%s\n",buffer);
+      }
+      else 
+      {
+        printf("end of file\n");
+        exit(0);
+      }
+    }
+  }
+
+}
+int main()
+{
+  wmorer();
+  return 0;
+}
