7a69cabe8dcd23fb63250073e3f3a03a63e5b981
zevin02 <1833195090@qq.com>
2022-04-28 01:25:54 +0800

    对server添加了往共享内存里面写入数据，在server中读取共享内存当中的数据，同时对共享内存没有调用System call做出了解释

--- procommunicate/sharemem/client.c
+++ procommunicate/sharemem/client.c
@@ -16,12 +16,26 @@ int main()
   int shmid=shmget(key,SIZE,0);//我们这里只需要获得就可以了
   //挂接上
   char* mem=(char*)shmat(shmid,NULL,0);
-
+  //这个地方就是我们要通信的区域
+  
+  //我们让client去写，但是共享内存可不是管道，他既可以写也可以读，随便写
+  char c='A';
+  while(c<='Z')
+  {
+    //我们认为共享内存他当前是一个长字符串
+    //我们就是在mem这个共有的空间里面进行操作,我们认为mem是一个长字符串
+    mem[c-'A']=c;//客户端不断向mem的0下标处写
+    c++;//这个字符串会不断的变长
+    mem[c-'A']=0;
+    sleep(1);
+  }
 
 
   //取消挂接
   shmdt(mem);
-
+  //sleep(5);
+  printf("client process detach sucess\n");
+  
 
   return 0;
 }
--- procommunicate/sharemem/server.c
+++ procommunicate/sharemem/server.c
@@ -20,16 +20,33 @@ int main()
     return 2;
   }
   printf("%d -> shm creat  success",shmid);
-  sleep(10);
+  //sleep(10);
   //让我们的进程和共享内存关联上
   char* mem=(char*)shmat(shmid,NULL,0);
   
   printf("attach success\n");
-  sleep(5);
+  //sleep(5);
   //相当于这前面都在构造函数里面
   //这里就是我们的通信逻辑
   
+  while(1)
+  {
+    sleep(1);
+    //这里我们有没有调用类似pipe or fifo 类似的read这样的接口呢？
+    //我们可以认为server是一个读取共享内存的程序
+    printf("%s\n",mem);//server认为共享内存里面放的是一个长字符串
+    //server不断 读取client向mem里面输入的东西
+    
+    //server或者client并没有调用系统调用接口，read或write
+    //所以共享内存一定建立好就可以直接映射到自己的进程地址空间里面，该进程就可以直接看到该共享内存，就如同我们malloc的空间，一般不需要如何系统调用接口，
+    //read或write的本质就是，将数据结构从内核拷贝给用户，或者从用户拷贝到内核
+    //
+    //当client没有启动的时候，server端根本不会因为client不写，而不读
+    //共享内存是所有的进程间通信的速度最快的
+    //
+    //共享内存不提供任何的同步或互斥机制，需要程序员自行保证数据的安全
   
+  }
 
 
 
@@ -37,13 +54,13 @@ int main()
   shmdt(mem);//去关联
 
   printf("detach success\n");
-  sleep(5);
+  //sleep(5);
 
   //最后不用的时候就手动的删掉就可以了
   shmctl(shmid,IPC_RMID,NULL);// 我们使用shmid把这个内存给删除掉，属性设置为NULL，
 
   printf("%d -> shm delete success",shmid);
-  sleep(10);
+  //sleep(10);
   
   return 0;
 }
