48c86a755f9e3d869c191516373afd5b497ed2a2
zevin02 <1833195090@qq.com>
2022-05-06 20:20:35 +0800

    简化代码

--- pthread_project/SPSC.cpp
+++ pthread_project/SPSC.cpp
@@ -11,12 +11,11 @@ using namespace std;
 class element
 {
 public:
-    int data; //放进去多少元素
-    long long id;   //生产者or消费者名字
-              //现在缓冲区里有多少元素
-    
+    int data;     //放进去多少元素
+    long long id; //生产者or消费者名字
+                  //现在缓冲区里有多少元素
 };
-class SPSCQueue//阻塞队列
+class SPSCQueue //阻塞队列
 {
 
 public:
@@ -37,36 +36,34 @@ public:
     void Push(element in)
     {
         //
-        //while (true)
-        //{
-            pthread_mutex_lock(&mutex);
-            while (_q.size() == _capacity)
-                pthread_cond_wait(&condP, &mutex);
-            _q.push(in);
-            pthread_mutex_unlock(&mutex);
-            //在这里我们不睡眠，要出现不规则的情况
-            pthread_cond_signal(&condC);
-        //}
+
+        pthread_mutex_lock(&mutex);
+        while (_q.size() == _capacity)
+            pthread_cond_wait(&condP, &mutex);
+        _q.push(in);
+        pthread_mutex_unlock(&mutex);
+        //在这里我们不睡眠，要出现不规则的情况
+        pthread_cond_signal(&condC);
     }
     void Pop()
     {
-        
-            pthread_mutex_lock(&mutex);
-            while (_q.empty())
-            {
-                pthread_cond_wait(&condC, &mutex);
-            }
-            //cout<<"pop"<<endl;
-            _q.pop();
-           // cout<<"finish pop"<<endl;
-            pthread_mutex_unlock(&mutex);
-            pthread_cond_signal(&condP);
-        //}
+
+        pthread_mutex_lock(&mutex);
+        while (_q.empty())
+        {
+            pthread_cond_wait(&condC, &mutex);
+        }
+
+        _q.pop();
+
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&condP);
     }
     element front()
     {
         return _q.front();
     }
+
 private:
     queue<element> _q;
     pthread_cond_t condC;
@@ -77,56 +74,44 @@ private:
 
 // template<class T>//我们把里面的类型都定义成t
 int now = 0;
-SPSCQueue  qt(5);
+SPSCQueue qt(100);
 void *produce(void *arg)
 {
 
     while (true)
     {
-        static int cnt=0;
+        static int cnt = 0;
         element t;
         int x = rand() % 50 + 1;
         t.data = x;
         now += x;
         t.id = pthread_self();
-        char* name=(char* )arg;                
-        // cout<<name<<" id="<<t.id<<" produce "<<t.data<<"products"<<" total ="<<now<<endl;
+        char *name = (char *)arg;
+
         qt.Push(t);
         cnt++;
-        // if(cnt%100==0)
-        // {
-        cout<<name<<" id="<<t.id<<" produce "<<t.data<<" products"<<" total ="<<now<<endl;
-            
-        // }
-      //  cout<<"1"<<endl;
+
+        cout << name << " id=" << t.id << " produce " << t.data << " products"
+             << " total =" << now << endl;
     }
     return NULL;
 }
 
-void * consume(void* arg)
+void *consume(void *arg)
 {
-    while(true)
+    while (true)
     {
-        static int n=0;
-        char* name=(char*)arg;
-       // cout<<"2"<<endl;
         
-        element t=qt.front();
-       // cout<<"3"<<endl;
-        
-
-        now-=t.data;
-      //  cout<<"5"<<endl;
-
+        char *name = (char *)arg;
+        element t = qt.front();
+        now -= t.data;
         qt.Pop();
-        n++;
-       // cout<<"4"<<endl;
-
-        // if(n%100==0)
-        cout<<name<<" consume "<<t.data<<"  "<<"  products "<<" total ="<<now<<endl;
+        
+        cout << name << " consume " << t.data << "  "
+             << "  products "
+             << " total =" << now << endl;
     }
 }
-
 
 int main()
 {
