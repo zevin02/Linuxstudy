25492062c62a12759154e5a5176117aa2710a570
zevin02 <1833195090@qq.com>
2022-05-29 17:16:17 +0800

    基于信号量的环形队列中的多生产者多消费者模型

--- pthread/pthread/ring_buffer/ring_cp.cc
+++ pthread/pthread/ring_buffer/ring_cp.cc
@@ -1,46 +1,49 @@
-#include"ring_queue.hpp"
-#include<pthread.h>
-#include<ctime>
-#include<cstdlib>
-#include<unistd.h>
+#include "ring_queue.hpp"
+#include <pthread.h>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
 using namespace ns_ring_queue;
 using std::cout;
 using std::endl;
 
-void* consumer(void* args)
+void *consumer(void *args)
 {
-    RingQueue<int>* rq=(RingQueue<int>*)args;
-    while(true)
+    RingQueue<int> *rq = (RingQueue<int> *)args;
+    while (true)
     {
-        int data=0;
+        int data = 0;
         rq->Pop(&data);
-        cout<<"消费的数据是"<<data<<std::endl;
+        cout << "消费的数据是" << data <<"我是"<<pthread_self()<< std::endl;
         sleep(1);
-        
     }
 }
-void* producter(void* args)
+void *producter(void *args)
 {
-    RingQueue<int>* rq=(RingQueue<int>*)args;
-    while(true)
+    RingQueue<int> *rq = (RingQueue<int> *)args;
+    while (true)
     {
-        int data=rand()%20+1;
-        cout<<"生产了数据是:"<<data<<std::endl;
+        int data = rand() % 20 + 1;
+        cout << "生产了数据是:" << data <<"我是"<<pthread_self()<< std::endl;
         rq->Push(data);
         sleep(1);
     }
 }
 
-
 int main()
 {
     //如果我们想要改成多生产者多消费者模型
-    srand((long long)time(nullptr));//种下随机数种子
-    pthread_t c,p;
-    RingQueue<int>* rq=new RingQueue<int>();//看到了同一个资源
-    pthread_create(&c,nullptr,consumer,(void*)rq);
-    pthread_create(&p,nullptr,producter,(void*)rq);
-    pthread_join(c,nullptr);
-    pthread_join(p,nullptr);
+    srand((long long)time(nullptr)); //种下随机数种子
+    pthread_t c, p;
+    pthread_t c1, p1;
+    RingQueue<int> *rq = new RingQueue<int>(); //看到了同一个资源
+    pthread_create(&c, nullptr, consumer, (void *)rq);
+    pthread_create(&p, nullptr, producter, (void *)rq);
+    pthread_create(&c1, nullptr, consumer, (void *)rq);
+    pthread_create(&p1, nullptr, producter, (void *)rq);
+    pthread_join(c, nullptr);
+    pthread_join(p, nullptr);
+    pthread_join(c1, nullptr);
+    pthread_join(p1, nullptr);
     return 0;
 }
--- pthread/pthread/ring_buffer/ring_queue.hpp
+++ pthread/pthread/ring_buffer/ring_queue.hpp
@@ -17,6 +17,7 @@ namespace ns_ring_queue
         sem_t _data;//初始化信号量
         int _c_step;
         int _p_step;
+        pthread_mutex_t _c_mtx,_p_mtx;
 
     public:
         RingQueue(int cap=g_cap_defualt)
@@ -27,38 +28,53 @@ namespace ns_ring_queue
          {
              sem_init(&_blank,0,cap);//一共有cap个空格子
              sem_init(&_data,0,0);//空格子数为0
+             pthread_mutex_init(&_c_mtx,nullptr);
+             pthread_mutex_init(&_p_mtx,nullptr);
          }
         ~RingQueue() {
             sem_destroy(&_data);
             sem_destroy(&_blank);
+            pthread_mutex_destroy(&_c_mtx);
+            pthread_mutex_destroy(&_p_mtx);
         }
         void Push(const T& in)
         {
-            //生产接口
-            
-            
+            //多生产者之间要互斥
+
+            //生产接口          
             //先预定资源
-            
+            //如果锁在前面，不是我们想要的那个样子，效率低，只允许一个执行流可以申请
             sem_wait(&_blank);//要成功申请一个格子资源，p(空位置)
+            //能进来的人一定是把信号量申请成功了
+            //多个生产者，可以预先分配给生产者，有锁进来，更新就可以了,这样的话，有人把锁释放了就可以立刻进行操作，
+            pthread_mutex_lock(&_p_mtx);
             //可以生产了，可是往哪个位置生产呢
             _ring_queue[_p_step]=in;
             //数据放进来，空位子少了，数据多了
-            sem_post(&_data);
             _p_step++;
             _p_step%=_cap;//不能超过总数
+            pthread_mutex_unlock(&_p_mtx);//保证互斥关系，生产者和生产者之间的互斥关系
+            sem_post(&_data);
+            
 
         }
         void Pop(T* out)
         {
+            //多消费者之间要互斥c_step就是临界资源
+                        
             //消费接口
             sem_wait(&_data);//如果这里一直为0，就一直等着，等到p进来
+            pthread_mutex_lock(&_c_mtx);
+            
             *out=_ring_queue[_c_step];
             //消费者取走了数据，这个地方就可以继续放数据了
-            sem_post(&_blank);
 
             _c_step++;
             _c_step%=_cap;
             //不能超过总数
+            pthread_mutex_unlock(&_c_mtx);//维护了消费者和消费者之间的互斥关系
+            sem_post(&_blank);
+
         }
     };
 }
